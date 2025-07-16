5388a45c1867a00a78dc472d1632e480ea21d72b
first2301 <121946223+first2301@users.noreply.github.com>
2025-07-16 16:06:39 +0900

    modi empty_Dag

--- dags/empty_dag.py
+++ dags/empty_dag.py
@@ -1,9 +1,11 @@
 import datetime
-from airflow.sdk import DAG
+from airflow.sdk import dag
 from airflow.providers.standard.operators.empty import EmptyOperator
 
-@dag(start_date=datetime.datetime(2025, 1, 1), schedule="@daily")
+
+@dag(start_date=datetime.datetime(2021, 1, 1), schedule="@daily")
 def generate_dag():
-    EmptyOperator(task_id="empty_operator")
+    EmptyOperator(task_id="task")
+
 
 generate_dag()
